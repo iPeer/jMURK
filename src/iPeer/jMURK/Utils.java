639de610f2ad023ca1b6d188495587dc9647f9cd
iPeer <ipeer@ipeerftw.co.cc>
2011-10-14 21:15:20 +0100

    Utilities

---
+++ src/iPeer/jMURK/Utils.java
@@ -0,0 +1,14 @@
+package iPeer.jMURK;
+
+import java.awt.*;
+
+public class Utils {
+
+	public Utils() { }
+	
+	public static Dimension resolution() {
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		return tk.getScreenSize();
+	}
+	
+}
