be31a1234c8cb93dde0805c13833424145c4a95a
iPeer <ipeer@ipeerftw.co.cc>
2011-10-27 12:41:30 +0100

    Settings & Default Weapon config.

---
+++ src/iPeer/jMURK/Settings.java
@@ -0,0 +1,12 @@
+package iPeer.jMURK;
+
+public class Settings {
+
+	public Settings() {
+	}
+	
+	public static boolean getBooleanSetting (String setting) {
+		return true; //TODO: Code.
+	}
+
+}
---
+++ src/iPeer/jMURK/item/Hands.java
@@ -0,0 +1,13 @@
+package iPeer.jMURK.item;
+
+public class Hands extends ItemWeapon {
+
+	public Hands() {
+		
+		name = "Hands";
+		minDam = 0;
+		maxDam = 3;
+		
+	}
+	
+}
